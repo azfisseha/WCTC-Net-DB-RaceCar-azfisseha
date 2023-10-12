25263395ac87d87521372219181267727f09d2e2
Adam Fisseha <adamfisseha@yahoo.com>
2023-10-12 18:40:57 -0500

    Program complete, pending a question about extra credit

---
+++ RaceTrack/Cars/Batmobile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Batmobile : RaceCar
+    {
+        public Batmobile() 
+        {
+            Name = "Batmobile";
+            TopSpeed = 140;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} hums to life.");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The flaps of the {Name} flare as it screeches to a halt.");
+            base.Brake();
+        }
+
+        public override void StopEngine() 
+        {
+            Console.WriteLine($"The {Name}'s engine whines down.");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/Batman.cs
@@ -0,0 +1,23 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Batman : Driver
+    {
+        public Batman(RaceCar car) : base(car)
+        {
+            Name = "Batman";
+            SkillLevel = 11;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
